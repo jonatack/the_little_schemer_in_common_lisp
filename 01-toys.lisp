fce44fd6f5b391311e64484679464ab2a5e8fc75
Jon Atack <jonnyatack@gmail.com>
2015-11-15 01:24:41 +0100

    Chapter 1 continued

--- 01-toys.lisp
+++ 01-toys.lisp
@@ -1,8 +1,24 @@
 ;;;; Chapter 1 of The Little Schemer:
 ;;;; Toys
 
-;;; The Law of cdr
+;;;; s-exps, atoms, lists, car, cdr, cons, null, eq
+
+;;; All atoms and lists are S-expressions.
+
+;;; An atom is a string of characters.
+
+(defun atom? (x)
+  (not (listp x)))
+
+;;; A list is a collection of S-expressions enclosed by parentheses.
+;;; A null (or empty) list contains zero S-expressions enclosed by parentheses.
+
+;;; The Law of Car
+;;; The primitive car is defined only for non-empty lists.
+
+;;; The Law of Cdr
 ;;; The primitive cdr is defined only for non-empty lists.
+;;; You cannot ask for the cdr of an atom.
 ;;; The cdr of any non-empty list is always another list.
 
 (defvar l '((b) (x y) ((c))))
@@ -10,7 +26,14 @@
 (cdr (cdr l)) ; (((c)))
 (cddr l)      ; (((c)))
 
-;;; cons takes two arguments:
-;;; the first one is any s-expression; the second one is any list.
+;;; The Law of Cons
+;;; The primitive `cons` takes two arguments.
+;;; The first one is any s-expression; the second one must be a list.
+;;; The result is a list.
 
+;;; The Law of Null
+;;; The primitive null is defined only for lists.
+
+;;; The Law of Eq
+;;; The primitive eq takes 2 arguments.
 
